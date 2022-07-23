ef3b7d2d4182e66e78bf2c2091f87aa2903f0355
Angel <angelavelarbonilla@gmail.com>
2022-07-23 15:55:17 -0400

    Changed site description, added contact info to welcome screen for mobile only

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,6 +8,14 @@ const Intro = () => {
         <h2>
           I'm Angel! I am currently a Software Engineer at Lockheed Martin.
         </h2>
+        <div className="contacts">
+          <div className="itemContainer">
+            <span>angelavelarbonilla@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <span>+1 609-605-1920</span>
+          </div>
+        </div>
       </div>
       <a href="#projects" alt="Down Arrow">
         <ArrowDropDown className="downArrow" />
