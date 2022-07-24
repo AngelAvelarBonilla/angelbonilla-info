52650fc959b7d2fc4a16262ba7ee6941029f78b5
Angel <angelavelarbonilla@gmail.com>
2022-07-24 15:09:37 -0400

    Added more info to experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -8,8 +8,9 @@ const Experience = () => {
       jobTitle: "Software Engineer",
       feats: [
         "Drove the containerization effort to build Docker images that are now used as lightweight build enviroments for 13 different services. This allowed developers to test their builds on their own machines.",
-
         "Ported a 32-bit codebase to 64-bit in response to a company wide effort to step up the operating systems used on our build machines.",
+        "Active security clearance at the Secret level",
+        "Skills learned - Java, Maven, Docker, C++, Unix Systems: RHEL and CentOS",
       ],
       date: "February 2022 - Present",
     },
@@ -21,6 +22,7 @@ const Experience = () => {
         "Provided integration support for ISVs looking to integrate their POS solutions with our REST Payment Gateway.",
         "Assisted IT/Development team in QA testing.",
         "Provided technical support for merchants using our applications like our FirstPay mSDK, 1stPayPOS/Mobile, and Virtual Terminal.",
+        "Skills learned - Payment Gateways, Postman",
       ],
       date: "May 2019 - January 2022",
     },
@@ -31,6 +33,7 @@ const Experience = () => {
       feats: [
         "Facilitated 2 Google-sponsored introductory Machine Learning workshops",
         "Hosted a handful of Cloud Study Jams focused on teaching others how to utilize the Google Cloud Platform, CSJ topics included: Cloud Essentials, Application Deployment, BigQuery, Firebase",
+        "Skills learned - Google Cloud Platform (App Engine, Big Data), Machine Learning",
       ],
       date: "June 2021 - May 2022",
     },
